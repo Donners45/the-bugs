0637681283248f9a3f29504bc160289ce2eadda8
Sean <stdonnelly@btinternet.com>
2016-12-05 21:37:21 +0000

    made some super basic style just to get going

--- src/js/app-boilerplate.js
+++ src/js/app-boilerplate.js
@@ -10,12 +10,15 @@ function appStartHandlers($fullPageWrapper){
 	var app = bugs();
 	
 	$fullPageWrapper.fullpage({
+		 anchors:['1', '2'],
 		//onLeave: function(index, nextIndex, direction){console.log(index, nextIndex, direction);},
 		afterLoad: function(anchorLink, index){
-				if (index === 2){
-					app.Start();
-				}
+			if (index === 2){
+				app.Start();
+			}else{
+				app.Terminate();
 			}
+		}
 	});
 
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,34 @@
 // main module 
 function bugs(){
+	var interval = null;
 
+	var rnd = function(){
+		return (Math.floor((Math.random() * 10) % 2) === 0);
+	};
+
+	var terminate = function(){
+		console.log('kill the bugs');
+		clearInterval(interval);
+	};
 
 	var start = function(){
 		var bug1 = 0;
 		var bug2 = 0;
-		var terminate = false;
-		do{
-			
-			if (Math.floor((Math.random() * 10) % 2) === 0) {bug1++;}
-			if (Math.floor((Math.random() * 10) % 2) === 0) {bug2++;}
-			
-			if(bug1 === 10 | bug2 === 10) {terminate = true;}
-
-		}while(terminate === false);
-		console.log(bug1, bug2);
+		
+		console.log('release the bugs');
+		interval = setInterval(function(){
+			console.log('step', bug1, bug2);
+			if (rnd()){bug1++;}
+			if (rnd()){bug2++;}
+			if(bug1 === 10 | bug2 === 10) {
+				terminate();
+				console.log(bug1, bug2);
+			}
+		}, 1300);
 	};
 	
-	
 	return {
-		Start: start
+		Start: start,
+		Terminate: terminate
 	};
 }
